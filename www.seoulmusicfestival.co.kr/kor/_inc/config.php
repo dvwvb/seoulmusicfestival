03bec2d38717f5389dd693bb4150a25defeadb71
y13564@gmail.com <y13564@gmail.com>
2022-10-07 17:24:06 +0900

    code modify

---
+++ www.seoulmusicfestival.co.kr/kor/_inc/config.php
@@ -0,0 +1,36 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
+    <meta name="format-detection" content="telephone=no" />
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="../../index.html">
+    <meta property="og:title" content="seoulmusicfestival">
+    <meta property="og:image" content="../../img/smuf_og.jpg">
+    <meta property="og:description" content="아름다운 감성으로 서울 일대를 가득 채울 뮤직 페스티벌이 개최됩니다.">
+    <meta name="description" content="아름다운 감성으로 서울 일대를 가득 채울 뮤직 페스티벌이 개최됩니다.">
+    <meta property="og:site_name" content="seoulmusicfestival">
+    <title>2022 SEOUL MUSIC FESTIVAL</title>
+    <!-- FONT -->
+    <link rel="stylesheet" href="../../font/font.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="../../utils/css/reset.css">
+    <link rel="stylesheet" href="../../utils/css/scroll.css">
+    <link rel="stylesheet" href="../../css/l.css">
+    <link rel="stylesheet" href="../../css/m.css">
+    <link rel="stylesheet" href="../../css/sb.css">
+
+    <!-- lib -->
+    <link rel="stylesheet" href="../../../cdn.jsdelivr.net/npm/swiper%408/swiper-bundle.min.css" />
+    <link href="../../../unpkg.com/aos%402.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../../cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link href="../../../cdn.jsdelivr.net/npm/remixicon%402.5.0/fonts/remixicon.css" rel="stylesheet">
+
+    <!-- plugin -->
+    <script src="../../../unpkg.com/aos%402.3.1/dist/aos.js"></script>
+
+    <!-- SCRIPT -->
+    <script src="../../utils/plugin/jquery.js"></script>
+    <script type="text/javascript" src="../../lib/js/itboard.js"></script>
+</head>
---
+++ www.seoulmusicfestival.co.kr/kor/_inc/header.php
@@ -0,0 +1,206 @@
+<header>
+    <div class="wrap" max="1400">
+
+        <ul class="logo">
+            <li>
+                <a href="https://seoulbeautymonth.or.kr/" target="_blank">
+                    <img src="../../img/logo/logo.png" alt="">
+                </a>
+            </li>
+            <li>
+                <a href="https://www.seoul.go.kr/main/index.jsp" target="_blank">
+                    <img src="../../img/logo/seoul_logoW.png" alt="">
+                </a>
+            </li>
+        </ul>
+
+
+        <ul class="gnb">
+            <li>
+                <a href="../index.html">HOME</a>
+            </li>
+            <li>
+                <a href="outline.html">SMUF 소개</a>
+                <ul class="lnb">
+                    <li>
+                        <a href="outline.html">행사 개요</a>
+                    </li>
+                    <li>
+                        <a href="schedule.html">일정표</a>
+                    </li>
+                    <li>
+                        <a href="map.html">Map</a>
+                    </li>
+                    <li>
+                        <a href="location.html">오시는 길</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li>
+                <a href="../lineup/lineup_1013.html">라인업</a>
+                <ul class="lnb">
+                    <li>
+                        <a href="../lineup/lineup_1013.html">라인업</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li>
+                <a href="../program/main_stage.html">프로그램</a>
+                <ul class="lnb">
+                    <li>
+                        <a href="../program/main_stage.html">MAIN STAGE</a>
+                    </li>
+                    <li>
+                        <a href="../program/sub_stage.html">SUB STAGE</a>
+                    </li>
+                    <li>
+                        <a href="../program/side_event.html">부대 프로그램</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li>
+                <a href="../gallery/gallery.html">갤러리</a>
+                <ul class="lnb">
+                    <li>
+                        <a href="../gallery/gallery.html">SMUF 2022</a>
+                    </li>
+                    <li>
+                        <a href="../gallery/smuf2019.html">SMUF 2019</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li>
+                <a href="../community/notice.html">커뮤니티</a>
+                <ul class="lnb">
+                    <li>
+                        <a href="../community/notice.html">공지사항</a>
+                    </li>
+                    <li>
+                        <a href="../community/inquiry.html">문의하기</a>
+                    </li>
+                    <li>
+                        <a href="../community/faq.html">FAQ</a>
+                    </li>
+                </ul>
+            </li>
+
+        </ul>
+
+
+        <div class="l_m">
+
+
+            <div class="loc">
+                <p>KOR <img src="../../img/icon/sh01.png" alt=""></p>
+                <ul class="hover">
+                    <li><a href="../../eng/index.html">ENG</a></li>
+                    <li><a href="../index.html">KOR</a></li>
+                </ul>
+            </div>
+
+
+
+            <div class="m_m">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="overBg">
+        <div class="wrap" max="1280">
+            <img src="../../img/header_img.png" alt="">
+        </div>
+    </div>
+
+    <div class="mobHeader">
+        <ul class="depth1">
+            <li>
+                <a href="../index.html">HOME</a>
+            </li>
+            <li>
+                <p><a href="outline.html">SMUF 소개</a> <i class="ri-add-line"></i></>
+                <ul class="depth2">
+                    <li>
+                        <a href="outline.html">행사 개요</a>
+                    </li>
+                    <li>
+                        <a href="schedule.html">일정표</a>
+                    </li>
+                    <li>
+                        <a href="map.html">Map</a>
+                    </li>
+                    <li>
+                        <a href="location.html">오시는 길</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li>
+                <p><a href="../lineup/lineup_1013.html">라인업</a> <i class="ri-add-line"></i></p>
+                <ul class="depth2">
+                    <li>
+                        <a href="../lineup/lineup_1013.html">라인업</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li>
+                <p><a href="../program/main_stage.html">프로그램</a> <i class="ri-add-line"></i></p>
+                <ul class="depth2">
+                    <li>
+                        <a href="../program/main_stage.html">MAIN STAGE</a>
+                    </li>
+                    <li>
+                        <a href="../program/sub_stage.html">SUB STAGE</a>
+                    </li>
+                    <li>
+                        <a href="../program/side_event.html">부대 프로그램</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li>
+                <p><a href="../gallery/gallery.html">갤러리</a> <i class="ri-add-line"></i></p>
+                <ul class="depth2">
+                    <li>
+                        <a href="../gallery/gallery.html">SMUF 2022</a>
+                    </li>
+                    <li>
+                        <a href="../gallery/smuf2019.html">SMUF 2019</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li>
+                <p><a href="../community/notice.html">커뮤니티</a> <i class="ri-add-line"></i></p>
+                <ul class="depth2">
+                    <li>
+                        <a href="../community/notice.html">공지사항</a>
+                    </li>
+                    <li>
+                        <a href="../community/inquiry.html">문의하기</a>
+                    </li>
+                    <li>
+                        <a href="../community/faq.html">FAQ</a>
+                    </li>
+                </ul>
+            </li>
+
+        </ul>
+    </div>
+
+</header>
+
+<script src="../../js/hea.js"></script>
+<script>
+    hE.init();
+</script>
+<div class="__sv"></div>
